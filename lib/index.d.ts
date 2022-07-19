d58e161842ba1b682fd7ff6569ad5bd0d8ae2e49
chefsale <sandi.fatic@gmail.com>
2022-07-19 13:09:29 +0100

    feat: add data types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,16 +7,38 @@ export class WalletData {
     publicKey: Uint8Array;
     signature: string;
     keyType: string;
+
+    constructor(accId: string, message: string, blockId: string, pubKey: string, sig: string, keyType: string);
+    serialize(): string;
+    static deserialize(serialized: string): WalletData;
 }
 
 export class CalimeroTokenData {
-        accountId: string;
-        shardId: string;
-        from: Date;
-        to: Date;
+    accountId: string;
+    shardId: string;
+    from: Date;
+    to: Date;
+
+    constructor(accountId, shardId, from, to);
+    serialize(): string;
+    static deserialize(serialized: string): CalimeroTokenData;
+
+    toHash(): string;
+    isDurationValid(): boolean;
 }
 
 export class CalimeroToken {
     walletData: WalletData;
     tokenData: CalimeroTokenData;
+  
+    constructor(walletData: WalletData, tokenData: CalimeroTokenData);
+
+    serialize(): string;
+    static deserialize(serialized: string): CalimeroTokenData;
+
+    toHash(): string;
+    verify(): boolean;
+
+    isSignatureValid(): boolean;
+    isDurationValid(): boolean;
 }

6a0394b90a967e01078feaffdf06c201f1a0ef46
Fran Domovic <fran@calimero.network>
2022-10-21 21:17:13 +0200

    feat sdk methods - setCredentials , confirm signIn

--- examples/calimero-login/src/calimeroSdk.js
+++ examples/calimero-login/src/calimeroSdk.js
@@ -1,7 +1,7 @@
 import { CalimeroSdk } from "calimero-auth-sdk";
 
 export default CalimeroSdk.init({
-    shardId: "iso-calimero-testnet",
+    shardId: "90-calimero-testnet",
     walletUrl: "https://testnet.mynearwallet.com",
     calimeroUrl: "https://api.development.calimero.network",
     calimeroWebSdkService: "https://api.development.calimero.network",
--- examples/calimero-login/src/components/Dashboard/Dashboard.js
+++ examples/calimero-login/src/components/Dashboard/Dashboard.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import calimeroSdk from "../../calimeroSdk";
 
 const PrivateComponent= () => <div>
-  <button onClick={calimeroSdk.syncAccount}>Sync Account</button>
   <button onClick={() => calimeroSdk.signTransaction("EAAAAGRhbGVwYXBpLnRlc3RuZXQAccIgton1dWYvHQfQnz1zBhZNus1OD84pxv%2Ftd4mpD17BM6EQAAAAABAAAABkYWxlcGFwaS50ZXN0bmV019g2Y1DPtOjGuld6oQ9tkKaS1X49bt%2BdSAs%2BTJ8bSiMBAAAAAwAAAKHtzM4bwtMAAAAAAAA%3D")}>Send Transaction</button>
   <button onClick={calimeroSdk.signOut}>Logout</button>
 </div>;
@@ -13,5 +12,9 @@ const PublicComponent = () => <div>
 </div>;
 
 export default function Dashboard() {
+  useEffect(()=>{
+    calimeroSdk.confirmSignIn();
+  },[])
+
   return calimeroSdk.isSignedIn() ? <PrivateComponent /> : <PublicComponent />;
 };
